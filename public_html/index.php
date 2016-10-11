0daca1db34af09b48d1e2ddedaea899b7be8b38b
Trevor Pelton <tnpflyer20@msn.com>
2016-10-11 13:36:47 -0600

    added bunches of stuff and image

--- public_html/index.php
+++ public_html/index.php
@@ -5,7 +5,22 @@
 		<title>Erm mah gerd ish a hermpage.</title>
 	</head>
 	<body>
-		<h1>Herro Wurld</h1>
+		<header>
+			<h1>Herro Wurld</h1>
+		</header>
+		<main>
+			<!--beginsection1 -->
+			<section>
+				<p>Lorem ipsum dolor sit amet, vis mutat epicurei adversarium id, cu est tota latine sensibus. Errem suscipit vituperata cu quo. Aeque noluisse ad ius, in nisl laudem qui. Sed novum consectetuer ne, sea inani volumus percipit ad. Ut eam sint putant accusamus.
+
+					Similique maiestatis at ius, nec et illud dolore commodo. Eu pri omnis putent, nam an dicta persius mediocritatem. Mucius delicatissimi eu qui. No nam lobortis salutatus delicatissimi, ut nec nulla audire. Eu mea numquam splendide omittantur, usu honestatis efficiantur cu. Est alia ferri et, semper ocurreret consetetur ea mei, ad nam vero saepe.
+				</p>
+				<img src="images/puppy.jpg" alt="Nova-puppy">
+			</section>
+			<section></section>
+			<section></section>
+		</main>
+
 	</body>
 </html>
 
