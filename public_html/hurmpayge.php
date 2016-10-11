e57d54df93cb5788ebd89bb567edfe32134c7029
Trevor Pelton <tnpflyer20@msn.com>
2016-10-11 11:52:33 -0600

    initial index creation

---
+++ public_html/hurmpayge.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fushi
+ * Date: 10/11/2016
+ * Time: 11:03 AM
+ */
---
+++ public_html/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<title>Erm mah gerd ish a hermpage.</title>
+	</head>
+	<body>
+		<h1>Herro Wurld</h1>
+	</body>
+</html>
+
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Fushi
+ * Date: 10/11/2016
+ * Time: 11:04 AM
+ */
+
